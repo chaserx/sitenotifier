55a28a16f5a78e56b5ccf0fd45b22bf25733c955
Chase Southard <chase.southard@gmail.com>
2011-07-17 17:58:47 -0400

    styling announcement area

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,5 +1,16 @@
 class Announcement < ActiveRecord::Base
   
+  scope :between_now, where("starts_at <= ? AND ends_at >= ?", Time.now, Time.now)
+  
+  def self.current_announcements(hide_time)
+    with_exclusive_scope(Announcement.between_now) do
+      if hide_time.nil?
+        #
+      else
+        #
+      end
+    end
+  end
   
   
 end
